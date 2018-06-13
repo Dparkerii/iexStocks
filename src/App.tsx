fb083709cacae0a5ab158e3967964e731c89ce6c
Bubba <drparkerii91@gmail.com>
2018-06-13 18:39:00 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
 import './App.css';
-
-import logo from './logo.svg';
+// import ph from './ph.png'
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+        <p className="App-body">
           To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p className="App-nav">
+          <h1 className="App-title">David Parker</h1>
+          <a href="mailto:drparkerii91@gmail.com">drparkerii91@gmail.com</a>
+          <p>Northern Virginia</p>
+          <p>703-507-3431</p>
         </p>
       </div>
     );
